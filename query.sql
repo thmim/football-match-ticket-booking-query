2d38100fa28a8b244ca54e1edd1a2dad784597ae
thmim <mdtaharimmim1010@gmail.com>
2026-06-25 16:01:24 +0600

    added four queries

--- query.sql
+++ query.sql
@@ -34,6 +34,30 @@ CREATE TABLE Bookings (
     
 );
 
+-- query-1
+select match_id,fixture,base_ticket_price from matches where match_status='Available' and tournament_category='Champions League';
+
+-- query-2
+select user_id,full_name,email from users where full_name ilike 'Tanvir%' or full_name ilike '%haque%';
+
+-- query-3
+select booking_id,user_id,match_id, coalesce(payment_status, 'Action Required') as systematic_status
+  from bookings
+  where payment_status is null;
+
+-- query-4
+select
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+from bookings as b
+join users as u
+    on b.user_id = u.user_id
+join matches as m
+    on b.match_id = m.match_id;
+
+
 
 
 
